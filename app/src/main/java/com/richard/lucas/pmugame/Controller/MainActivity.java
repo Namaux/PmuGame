0cc4619ba4716aca3f63b1dbcea0ba21fda0c35c
Lucas <richard.lucas.62240@gmail.com>
2018-12-10 16:14:17 +0100

    Initial commit

---
+++ app/src/main/java/com/richard/lucas/pmugame/Controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.richard.lucas.pmugame.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.richard.lucas.pmugame.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
